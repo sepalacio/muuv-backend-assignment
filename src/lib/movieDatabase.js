362c77650118d990dbca93c5e73bac7b352bbda3
Sebastian Palacio <spalacio@namutek.com>
2022-09-03 21:12:47 -0500

    feat: add lib for consuming The Movie Database API

---
+++ src/lib/movieDatabase.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+const { MOVIE_DATABASE_API_KEY } = process.env;
+const movieDatabaseConfig = {
+  host: 'https://api.themoviedb.org',
+  version: 3,
+  language: 'en-US',
+};
+
+const getUrl = ({ host, version, language }, path) => (
+  `${host}/${version}/${path}?api_key=${MOVIE_DATABASE_API_KEY}&language=${language}`
+);
+
+/**
+ * Perform API request to the movie database service
+ * @param {object} options
+ * @param {string} options.method http verb 'get' 'post' 'put'
+ * @param {string} options.path path to the desired api service
+ * @param {object} options.data request data
+ * @return {Promise<Object>}
+ */
+const MovieDatabase = async ({ method, path, data }) => {
+  const url = getUrl(movieDatabaseConfig, path);
+
+  const { data: response } = await axios({
+    method,
+    url,
+    data,
+  });
+
+  return response;
+};
+
+module.exports = MovieDatabase;
