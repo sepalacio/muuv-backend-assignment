961f8584fc56d98c5bef43c86deadb52c58a30e5
Sebastian Palacio <spalacio@namutek.com>
2022-09-05 20:25:16 -0500

    perf: add clustering to app server - Increase app concurrency support by adding nodeJs cluster mode

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,6 @@
 const chalk = require('chalk');
+const cluster = require('cluster');
+const totalCPUs = require('os').cpus().length;
 
 const { name, version } = require('../../package.json');
 const { port } = require('../config/server');
@@ -10,14 +12,31 @@ const { NODE_ENV } = process.env;
 
 const initServer = () => {
   try {
-    addAppListeners();
-    app.listen(port, () => {
-      console.info(chalk.green(`
-        ${name} ${version}
-        Server running on http://localhost:${port}/
-        Running with NodeJS ${process.version}
-        Environment: ${NODE_ENV}`));
-    });
+    if (cluster.isMaster) {
+      console.log(`Number of CPUs is ${totalCPUs}`);
+      console.log(`Master ${process.pid} is running`);
+
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < totalCPUs; i++) {
+        cluster.fork();
+      }
+
+      cluster.on('exit', (worker) => {
+        console.log(`worker ${worker.process.pid} died`);
+        cluster.fork();
+      });
+    } else {
+      console.log(`Worker ${process.pid} started`);
+
+      addAppListeners();
+      app.listen(port, () => {
+        console.info(chalk.green(`
+          ${name} ${version}
+          Server running on http://localhost:${port}/
+          Running with NodeJS ${process.version}
+          Environment: ${NODE_ENV}`));
+      });
+    }
   } catch (error) {
     handleFatalError();
   }
